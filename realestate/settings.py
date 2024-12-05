7b535ada5a6a99704513d7c4b38b0f1a1b29fc03
yeonana <135162450+yeonana@users.noreply.github.com>
2024-12-05 16:53:58 -0600

    update allowed host

--- realestate/settings.py
+++ realestate/settings.py
@@ -33,7 +33,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
 
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://dream-nest-839d524209f3.herokuapp.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "dream-nest-839d524209f3.herokuapp.com/"]
 
 
 # Application definition
