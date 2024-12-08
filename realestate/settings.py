5dd4ae13341bc96664f92ca8b4ed800fd1799f64
yeonana <135162450+yeonana@users.noreply.github.com>
2024-12-08 14:38:49 -0600

    setting

--- realestate/settings.py
+++ realestate/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-06*4$32yorrc1d45vdodrw!h1xkp-syk%$ha*o-71ql$z7xgw%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
