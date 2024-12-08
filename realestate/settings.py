aec16c38338c5bf784ae09b3a577e01bee964031
yeonana <135162450+yeonana@users.noreply.github.com>
2024-12-08 16:47:04 -0600

    change debug

--- realestate/settings.py
+++ realestate/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
